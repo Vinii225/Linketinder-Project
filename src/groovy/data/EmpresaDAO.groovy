9ab61b5d9c496046d36644a130df8a6bdc90ba7f
Vinícius Ares <vinicius.ares12@gmail.com>
2026-04-26 20:24:35 -0300

    refactor: uso de withCloseable e remoção de duplicação com método

--- src/groovy/data/EmpresaDAO.groovy
+++ src/groovy/data/EmpresaDAO.groovy
@@ -4,6 +4,7 @@ import groovy.model.Empresa
 
 import java.sql.Connection
 import java.sql.PreparedStatement
+import java.sql.ResultSet
 
 class EmpresaDAO {
 
@@ -14,23 +15,23 @@ class EmpresaDAO {
             RETURNING id_empresa
         """.stripIndent()
 
-        Connection connection = DatabaseConnection.getConnection()
-        PreparedStatement statement = connection.prepareStatement(sql)
-        statement.setString(1, empresa.nomeEmpresa)
-        statement.setString(2, empresa.cnpj)
-        statement.setString(3, empresa.emailCorporativo)
-        statement.setString(4, empresa.descricaoEmpresa)
-        statement.setString(5, empresa.pais)
-        statement.setString(6, empresa.cep)
-        statement.setString(7, empresa.senha)
+        DatabaseConnection.getConnection().withCloseable { Connection connection ->
+            connection.prepareStatement(sql).withCloseable { PreparedStatement statement ->
+                statement.setString(1, empresa.nomeEmpresa)
+                statement.setString(2, empresa.cnpj)
+                statement.setString(3, empresa.emailCorporativo)
+                statement.setString(4, empresa.descricaoEmpresa)
+                statement.setString(5, empresa.pais)
+                statement.setString(6, empresa.cep)
+                statement.setString(7, empresa.senha)
 
-        def resultSet = statement.executeQuery()
-        resultSet.next()
-        empresa.idEmpresa = resultSet.getInt("id_empresa")
+                statement.executeQuery().withCloseable { ResultSet resultSet ->
+                    resultSet.next()
+                    empresa.idEmpresa = resultSet.getInt("id_empresa")
+                }
+            }
+        }
 
-        resultSet.close()
-        statement.close()
-        connection.close()
         return empresa
     }
 
@@ -42,26 +43,17 @@ class EmpresaDAO {
         """.stripIndent()
 
         List<Empresa> empresas = []
-        Connection connection = DatabaseConnection.getConnection()
-        PreparedStatement statement = connection.prepareStatement(sql)
-        def resultSet = statement.executeQuery()
 
-        while (resultSet.next()) {
-            empresas << new Empresa(
-                idEmpresa: resultSet.getInt("id_empresa"),
-                nomeEmpresa: resultSet.getString("nome_empresa"),
-                cnpj: resultSet.getString("cnpj"),
-                emailCorporativo: resultSet.getString("email_corporativo"),
-                descricaoEmpresa: resultSet.getString("descricao_empresa"),
-                pais: resultSet.getString("pais"),
-                cep: resultSet.getString("cep"),
-                senha: resultSet.getString("senha")
-            )
+        DatabaseConnection.getConnection().withCloseable { Connection connection ->
+            connection.prepareStatement(sql).withCloseable { PreparedStatement statement ->
+                statement.executeQuery().withCloseable { ResultSet resultSet ->
+                    while (resultSet.next()) {
+                        empresas << mapEmpresa(resultSet)
+                    }
+                }
+            }
         }
 
-        resultSet.close()
-        statement.close()
-        connection.close()
         return empresas
     }
 
@@ -72,29 +64,20 @@ class EmpresaDAO {
             WHERE id_empresa = ?
         """.stripIndent()
 
-        Connection connection = DatabaseConnection.getConnection()
-        PreparedStatement statement = connection.prepareStatement(sql)
-        statement.setInt(1, idEmpresa)
-
-        def resultSet = statement.executeQuery()
         Empresa empresa = null
 
-        if (resultSet.next()) {
-            empresa = new Empresa(
-                idEmpresa: resultSet.getInt("id_empresa"),
-                nomeEmpresa: resultSet.getString("nome_empresa"),
-                cnpj: resultSet.getString("cnpj"),
-                emailCorporativo: resultSet.getString("email_corporativo"),
-                descricaoEmpresa: resultSet.getString("descricao_empresa"),
-                pais: resultSet.getString("pais"),
-                cep: resultSet.getString("cep"),
-                senha: resultSet.getString("senha")
-            )
+        DatabaseConnection.getConnection().withCloseable { Connection connection ->
+            connection.prepareStatement(sql).withCloseable { PreparedStatement statement ->
+                statement.setInt(1, idEmpresa)
+
+                statement.executeQuery().withCloseable { ResultSet resultSet ->
+                    if (resultSet.next()) {
+                        empresa = mapEmpresa(resultSet)
+                    }
+                }
+            }
         }
 
-        resultSet.close()
-        statement.close()
-        connection.close()
         return empresa
     }
 
@@ -105,33 +88,51 @@ class EmpresaDAO {
             WHERE id_empresa = ?
         """.stripIndent()
 
-        Connection connection = DatabaseConnection.getConnection()
-        PreparedStatement statement = connection.prepareStatement(sql)
-        statement.setString(1, empresa.nomeEmpresa)
-        statement.setString(2, empresa.cnpj)
-        statement.setString(3, empresa.emailCorporativo)
-        statement.setString(4, empresa.descricaoEmpresa)
-        statement.setString(5, empresa.pais)
-        statement.setString(6, empresa.cep)
-        statement.setString(7, empresa.senha)
-        statement.setInt(8, empresa.idEmpresa)
+        int updated = 0
 
-        int updated = statement.executeUpdate()
-        statement.close()
-        connection.close()
+        DatabaseConnection.getConnection().withCloseable { Connection connection ->
+            connection.prepareStatement(sql).withCloseable { PreparedStatement statement ->
+                statement.setString(1, empresa.nomeEmpresa)
+                statement.setString(2, empresa.cnpj)
+                statement.setString(3, empresa.emailCorporativo)
+                statement.setString(4, empresa.descricaoEmpresa)
+                statement.setString(5, empresa.pais)
+                statement.setString(6, empresa.cep)
+                statement.setString(7, empresa.senha)
+                statement.setInt(8, empresa.idEmpresa)
+
+                updated = statement.executeUpdate()
+            }
+        }
+
         return updated > 0
     }
 
     boolean delete(Integer idEmpresa) {
         String sql = "DELETE FROM empresa WHERE id_empresa = ?"
 
-        Connection connection = DatabaseConnection.getConnection()
-        PreparedStatement statement = connection.prepareStatement(sql)
-        statement.setInt(1, idEmpresa)
+        int deleted = 0
 
-        int deleted = statement.executeUpdate()
-        statement.close()
-        connection.close()
+        DatabaseConnection.getConnection().withCloseable { Connection connection ->
+            connection.prepareStatement(sql).withCloseable { PreparedStatement statement ->
+                statement.setInt(1, idEmpresa)
+                deleted = statement.executeUpdate()
+            }
+        }
+
         return deleted > 0
+    }
+
+    private static Empresa mapEmpresa(ResultSet resultSet) {
+        return new Empresa(
+            idEmpresa: resultSet.getInt("id_empresa"),
+            nomeEmpresa: resultSet.getString("nome_empresa"),
+            cnpj: resultSet.getString("cnpj"),
+            emailCorporativo: resultSet.getString("email_corporativo"),
+            descricaoEmpresa: resultSet.getString("descricao_empresa"),
+            pais: resultSet.getString("pais"),
+            cep: resultSet.getString("cep"),
+            senha: resultSet.getString("senha")
+        )
     }
 }
